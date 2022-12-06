2c8ab2f1e1d1127e51800f3a0a58f3d4f0ded263
Jonathan Stevenson <jonathan@greatfigure8.com>
2022-12-06 10:32:14 -0600

    Working Commit

--- ui/pages/api/users/login.ts
+++ ui/pages/api/users/login.ts
@@ -1,4 +1,30 @@
+import _ from 'lodash'
+import { NextApiRequest, NextApiResponse } from 'next'
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {}
+) {
+  const authErrorStatement = "UNAUTH";
+
+  try {
+    if (req.method !== "POST")
+      throw new Error(
+        `The method ${req.method} is not supported for this endpoint.  Please use POST`
+      );
+    const { userName, password } = req.body;
+    if (_.isNil(userName) || _.isNil(password))
+      throw new Error(authErrorStatement);
+
+    if (userName === "test" && password === "test") {
+      res.status(200).json({ message: "AUTHORIZED" });
+    } else {
+      throw new Error(authErrorStatement);
+    }
+  } catch (e) {
+    const err = e as Error;
+    if (err.message === authErrorStatement) {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  }
+}
