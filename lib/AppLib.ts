d8a620b854b45028f2ddc893b291f297e7744193
Jonathan Stevenson <jsteve81@gmail.com>
2022-12-19 16:47:12 -0600

    blah

--- lib/AppLib.ts
+++ lib/AppLib.ts
@@ -6,157 +6,188 @@ import Env from './Env'
 import FusionResponse from './models/fusion/FusionResponse'
 import FusionUserAccount from './models/fusion/FusionUserAccount'
 import FusionUserRole from './models/fusion/FusionUserRole'
+import FusionWorker from './models/fusion/FusionWorker'
 import OrdsRole from './models/ords/OrdsRole'
 import UserRoles from './models/UserRoles'
 
 export type oracleParams = {
-  onlyData: boolean;
-  limit: number;
-  q?: string | null;
-  fields: string | null;
+    onlyData: boolean;
+    limit: number;
+    q?: string | null;
+    fields: string | null;
 };
 
 export default class AppLib {
-  //#region static methods
-  public static getQueryParamValue(
-    value: string | string[] | undefined
-  ): string {
-    if (_.isUndefined(value)) return '';
-    const firstValue = Array.isArray(value) ? _.first(value) : value;
-    return _.isUndefined(firstValue) ? '' : firstValue;
-  }
-  public static makeAxiosErrorResponse({
-    e,
-    res,
-  }: {
-    e: any;
-    res: NextApiResponse;
-  }): void {
-    const axiosError = e as AxiosError;
-    res.status(400).json({
-      error: axiosError.response?.data,
-      response: {
-        status: axiosError.response?.status,
-        statusText: axiosError.response?.statusText,
-        config: {
-          url: axiosError.response?.config.url,
-          passedData: axiosError.response?.config.data,
-        },
-      },
-    });
-  }
-  public static getOracleUserNameSearchParams({
-    userName,
-    onlyData,
-  }: {
-    userName: string;
-    onlyData: boolean;
-  }): oracleParams {
-    return {
-      q: `Username eq '${userName}'`,
-      fields: 'PersonId,PersonNumber,UserId,Username,GUID',
-      onlyData,
-      limit: 500,
-    };
-  }
-  public static getOracleWorkerSearchParams(): oracleParams {
-    return {
-      fields: 'PersonNumber,PersonId;names:DisplayName',
-      onlyData: true,
-      limit: 500,
-    };
-  }
-
-  //#endregion
-
-  env: Env;
-  axiosOracle: AxiosInstance;
-  axiosOrds: AxiosInstance;
-  constructor() {
-    this.env = new Env();
-    this.axiosOracle = axios.create({
-      baseURL: this.env.urls.oracle,
-      auth: {
-        username: this.env.auth.oracle.userName,
-        password: this.env.auth.oracle.password,
-      },
-    });
-    this.axiosOrds = axios.create({
-      baseURL: this.env.urls.ords,
-    });
-  }
-
-  public async getOracleUser(userName: string): Promise<FusionUserAccount> {
-    const params = AppLib.getOracleUserNameSearchParams({
-      userName,
-      onlyData: false,
-    });
-    const userResp = await this.axiosOracle.get<FusionUserAccount>(
-      this.env.actions.oracle.userAccounts,
-      { params }
-    );
-
-    if (_.isNil(userResp.data.GUID)) {
-      throw new Error(
-        `A user with the user name of ${userName} was not found.`
-      );
-    } else {
-      return userResp.data;
+    //#region static methods
+    public static getQueryParamValue(
+        value: string | string[] | undefined,
+    ): string {
+        if (_.isUndefined(value)) return '';
+        const firstValue = Array.isArray(value) ? _.first(value) : value;
+        return _.isUndefined(firstValue) ? '' : firstValue;
     }
-  }
-
-  public async tieUserAndEmp({
-    userGuid,
-    workerPersonId,
-  }: {
-    userGuid: string | undefined;
-    workerPersonId: string | undefined;
-  }): Promise<FusionUserAccount> {
-    const response = await this.axiosOracle.post<FusionUserAccount>(
-      'api/users',
-      {
-        userGuid: userGuid,
-        workerPersId: workerPersonId,
-      }
-    );
-    return response.data;
-  }
-
-  public async getUserRoles(userName: string): Promise<UserRoles> {
-    const oracleUserResp = await this.axiosOracle.get<
-      FusionResponse<FusionUserAccount>
-    >(this.env.actions.oracle.userAccounts, {
-      params: AppLib.getOracleUserNameSearchParams({
+    public static makeAxiosErrorResponse({
+        e,
+        res,
+    }: {
+        e: any;
+        res: NextApiResponse;
+    }): void {
+        const axiosError = e as AxiosError;
+        res.status(400).json({
+            error: axiosError.response?.data,
+            response: {
+                status: axiosError.response?.status,
+                statusText: axiosError.response?.statusText,
+                config: {
+                    url: axiosError.response?.config.url,
+                    passedData: axiosError.response?.config.data,
+                },
+            },
+        });
+    }
+    public static getOracleUserNameSearchParams({
         userName,
-        onlyData: false,
-      }),
-    });
-
-    FusionResponse.validateResponse(oracleUserResp.data, true);
-    const roleLink = FusionUserAccount.getUserAccountLink(
-      oracleUserResp.data.items[0].links
-    );
-    let oraRoleData = new FusionResponse<FusionUserRole>();
-    if (!_.isNil(roleLink)) {
-      const oraRoleResponse = await this.axiosOracle.get<
-        FusionResponse<FusionUserRole>
-      >(roleLink.href);
-      FusionResponse.validateResponse(oraRoleResponse.data, true);
-      oraRoleData = oraRoleResponse.data;
+        onlyData,
+    }: {
+        userName: string;
+        onlyData: boolean;
+    }): oracleParams {
+        return {
+            q: `Username eq '${userName}'`,
+            fields: 'PersonId,PersonNumber,UserId,Username,GUID',
+            onlyData,
+            limit: 500,
+        };
     }
-    const dbRoleResponse = await this.axiosOrds.get<FusionResponse<OrdsRole>>(
-      '/',
-      { params: { q: `{"user_name":"${userName}}"}` } }
-    );
-    const dbRespValid = FusionResponse.validateResponse(
-      dbRoleResponse.data,
-      false
-    );
-    const userRoles = new UserRoles();
-    userRoles.dbRoles = dbRespValid ? dbRoleResponse.data.items : null;
-    userRoles.fusionRoles = oraRoleData.items;
-    userRoles.userName = userName;
+    public static getOracleWorkerSearchParams(): oracleParams {
+        return {
+            fields: 'PersonNumber,PersonId;names:DisplayName',
+            onlyData: true,
+            limit: 500,
+        };
+    }
 
-    return userRoles;
-  }
+    //#endregion
+
+    env: Env;
+    axiosOracle: AxiosInstance;
+    axiosOrds: AxiosInstance;
+    constructor() {
+        this.env = new Env();
+        this.axiosOracle = axios.create({
+            baseURL: this.env.urls.oracle,
+            auth: {
+                username: this.env.auth.oracle.userName,
+                password: this.env.auth.oracle.password,
+            },
+        });
+        this.axiosOrds = axios.create({
+            baseURL: this.env.urls.ords,
+        });
+    }
+
+    public async getOracleUser(userName: string): Promise<FusionUserAccount> {
+        const params = AppLib.getOracleUserNameSearchParams({
+            userName,
+            onlyData: false,
+        });
+        const userResp = await this.axiosOracle.get<FusionUserAccount>(
+            this.env.actions.oracle.userAccounts,
+            { params },
+        );
+
+        if (_.isNil(userResp.data.GUID)) {
+            throw new Error(
+                `A user with the user name of ${userName} was not found.`,
+            );
+        } else {
+            return userResp.data;
+        }
+    }
+
+    public async tieUserAndEmp({
+        userGuid,
+        workerPersonId,
+    }: {
+        userGuid: string | undefined;
+        workerPersonId: string | undefined;
+    }): Promise<FusionUserAccount> {
+        const response = await this.axiosOracle.post<FusionUserAccount>(
+            'api/users',
+            {
+                userGuid: userGuid,
+                workerPersId: workerPersonId,
+            },
+        );
+        return response.data;
+    }
+
+    public async getFusionUserByGuid(
+        guid: string,
+    ): Promise<FusionUserAccount | null> {
+        const userResponse = await this.axiosOracle.get<
+            FusionResponse<FusionUserAccount>
+        >(`${this.env.actions.oracle.userAccounts}/${guid}`, {
+            params: { onlyData: true },
+        });
+
+        if (userResponse && userResponse.data && userResponse.data.count > 0) {
+            const user = _.first(userResponse.data.items);
+            return !_.isNil(user) ? user : null;
+        }
+        throw new Error(
+            `There were no users returned from the call to ${this.env.actions.oracle.userAccounts}/${guid}`,
+        );
+    }
+
+    public async getWorkerByPersonId(
+        workerPersId: string,
+    ): Promise<FusionWorker> {
+        const response = await this.axiosOracle.get<
+            FusionResponse<FusionWorker>
+        >(this.env.actions.oracle.workers, {
+            params: {
+                q: `PersonId eq ${workerPersId}`,
+                fields: 'PersonId,PersonNumber',
+            },
+        });
+    }
+
+    public async getUserRoles(userName: string): Promise<UserRoles> {
+        const oracleUserResp = await this.axiosOracle.get<
+            FusionResponse<FusionUserAccount>
+        >(this.env.actions.oracle.userAccounts, {
+            params: AppLib.getOracleUserNameSearchParams({
+                userName,
+                onlyData: false,
+            }),
+        });
+
+        FusionResponse.validateResponse(oracleUserResp.data, true);
+        const roleLink = FusionUserAccount.getUserAccountLink(
+            oracleUserResp.data.items[0].links,
+        );
+        let oraRoleData = new FusionResponse<FusionUserRole>();
+        if (!_.isNil(roleLink)) {
+            const oraRoleResponse = await this.axiosOracle.get<
+                FusionResponse<FusionUserRole>
+            >(roleLink.href);
+            FusionResponse.validateResponse(oraRoleResponse.data, true);
+            oraRoleData = oraRoleResponse.data;
+        }
+        const dbRoleResponse = await this.axiosOrds.get<
+            FusionResponse<OrdsRole>
+        >('/', { params: { q: `{"user_name":"${userName}}"}` } });
+        const dbRespValid = FusionResponse.validateResponse(
+            dbRoleResponse.data,
+            false,
+        );
+        const userRoles = new UserRoles();
+        userRoles.dbRoles = dbRespValid ? dbRoleResponse.data.items : null;
+        userRoles.fusionRoles = oraRoleData.items;
+        userRoles.userName = userName;
+
+        return userRoles;
+    }
 }
--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -2,98 +2,101 @@ import axios, { AxiosError } from 'axios'
 import _ from 'lodash'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import libs, { fusionUserAccount, oracleResponse } from '../../../lib/AppLib'
+import AppLib from '../../../lib/AppLib'
+import FusionResponse from '../../../lib/models/fusion/FusionResponse'
+import FusionUserAccount from '../../../lib/models/fusion/FusionUserAccount'
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<fusionUserAccount | undefined | unknown>
+    req: NextApiRequest,
+    res: NextApiResponse<FusionUserAccount | undefined | unknown>,
 ) {
-  libs.methods.setAxiosDefaults();
+    const appLib = new AppLib();
+    try {
+        if (req.method !== 'POST')
+            throw new Error(
+                `${req.method} is not supported for this endpoint.  Please use a post`,
+            );
+        const { userGuid, workerPersId } = req.body;
+        if (_.isNil(userGuid) || _.isNil(workerPersId))
+            throw new Error(
+                'The request body must look like the following {"userGuid": string, "personId", number}',
+            );
 
-  try {
-    if (req.method !== 'POST')
-      throw new Error(
-        `${req.method} is not supported for this endpoint.  Please use a post`
-      );
-    const { userGuid, workerPersId } = req.body;
-    if (_.isNil(userGuid) || _.isNil(workerPersId))
-      throw new Error(
-        'The request body must look like the following {"userGuid": string, "personId", number}'
-      );
+        const userCheck = await appLib.getFusionUserByGuid(userGuid);
+        if (
+            _.isNil(userCheck) ||
+            _.isNil(userCheck.GUID) ||
+            userCheck.GUID !== userGuid
+        )
+            throw new Error(`The user account GUID ${userGuid} was not found`);
 
-    const userCheck = await axios.get<fusionUserAccount>(
-      `${libs.constants.actions.userAccounts}/${userGuid}?onlyData=true`
-    );
-    if (_.isNil(userCheck.data.GUID) || userCheck.data.GUID !== userGuid)
-      throw new Error(`The user account GUID ${userGuid} was not found`);
+        const personCheck = await axios.get<FusionResponse<Worker>>(
+            `${libs.constants.actions.workers}/?onlyData=true&q=PersonId=${workerPersId}&fields=PersonId,PersonNumber`,
+        );
 
-    const personCheck = await axios.get<oracleResponse<Worker>>(
-      `${libs.constants.actions.workers}/?onlyData=true&q=PersonId=${workerPersId}&fields=PersonId,PersonNumber`
-    );
-
-    if (_.isEmpty(personCheck.data.items)) {
-      throw new Error(`The person with id ${workerPersId} was not found`);
-    }
-
-    const targetPersUserResp = await axios.get<
-      oracleResponse<fusionUserAccount>
-    >(
-      `${libs.constants.actions.userAccounts}/?onlyData=true&q=PersonId=${workerPersId}`
-    );
-
-    const targetUser = userCheck.data;
-    const targetPerUsrAcct =
-      targetPersUserResp.data.count === 1
-        ? targetPersUserResp.data.items[0]
-        : null;
-
-    // set both account to have no person record
-    await axios.patch(
-      `${libs.constants.actions.userAccounts}/${targetUser.GUID}`,
-      {
-        PersonId: null,
-      }
-    );
-    // only set this one to null if it isn't already null
-    if (!_.isNil(targetPerUsrAcct)) {
-      await axios.patch(
-        `${libs.constants.actions.userAccounts}/${targetPerUsrAcct.GUID}`,
-        {
-          PersonId: null,
+        if (_.isEmpty(personCheck.data.items)) {
+            throw new Error(`The person with id ${workerPersId} was not found`);
         }
-      );
+
+        const targetPersUserResp = await axios.get<
+            FusionResponse<FusionUserAccount>
+        >(
+            `${libs.constants.actions.userAccounts}/?onlyData=true&q=PersonId=${workerPersId}`,
+        );
+
+        const targetUser = userCheck.data;
+        const targetPerUsrAcct =
+            targetPersUserResp.data.count === 1
+                ? targetPersUserResp.data.items[0]
+                : null;
+
+        // set both account to have no person record
+        await axios.patch(
+            `${libs.constants.actions.userAccounts}/${targetUser.GUID}`,
+            {
+                PersonId: null,
+            },
+        );
+        // only set this one to null if it isn't already null
+        if (!_.isNil(targetPerUsrAcct)) {
+            await axios.patch(
+                `${libs.constants.actions.userAccounts}/${targetPerUsrAcct.GUID}`,
+                {
+                    PersonId: null,
+                },
+            );
+        }
+        // set the person record to the user account of the current user
+        await axios.patch<FusionUserAccount>(
+            `${libs.constants.actions.userAccounts}/${targetUser.GUID}`,
+            {
+                PersonId: workerPersId,
+            },
+        );
+
+        // set the suspended flag of the user we just added this account to
+        // as not suspenede
+        const userResponse = await axios.patch<FusionUserAccount>(
+            `${libs.constants.actions.userAccounts}/${targetUser.GUID}`,
+            {
+                SuspendedFlag: false,
+            },
+        );
+
+        res.status(200).json(userResponse.data);
+    } catch (e) {
+        console.log(e);
+        const axiosError = e as AxiosError;
+        res.status(400).json({
+            error: axiosError.response?.data,
+            response: {
+                status: axiosError.response?.status,
+                statusText: axiosError.response?.statusText,
+                config: {
+                    url: axiosError.response?.config.url,
+                    passedData: axiosError.response?.config.data,
+                },
+            },
+        });
     }
-    // set the person record to the user account of the current user
-    await axios.patch<fusionUserAccount>(
-      `${libs.constants.actions.userAccounts}/${targetUser.GUID}`,
-      {
-        PersonId: workerPersId,
-      }
-    );
-
-    // set the suspended flag of the user we just added this account to
-    // as not suspenede
-    const userResponse = await axios.patch<fusionUserAccount>(
-      `${libs.constants.actions.userAccounts}/${targetUser.GUID}`,
-      {
-        SuspendedFlag: false,
-      }
-    );
-
-    res.status(200).json(userResponse.data);
-  } catch (e) {
-    console.log(e);
-    const axiosError = e as AxiosError;
-    res.status(400).json({
-      error: axiosError.response?.data,
-      response: {
-        status: axiosError.response?.status,
-        statusText: axiosError.response?.statusText,
-        config: {
-          url: axiosError.response?.config.url,
-          passedData: axiosError.response?.config.data,
-        },
-      },
-    });
-  }
 }
